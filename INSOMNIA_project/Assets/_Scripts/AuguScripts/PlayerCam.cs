239f3aeeb638d780140f22e3903e2999c55a4b87
AuguZer <augumahieu@hotmail.fr>
2023-09-04 18:13:33 +0200

    Simple Base Player Movement
    Move + Look
    Sprint
    Crouch

---
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerCam.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCam : MonoBehaviour
+{
+    [SerializeField] InputActionAsset inputActions;
+
+    [SerializeField] float sensX;
+    [SerializeField] float sensY;
+
+    public Transform playerBody;
+
+    float xRotation;
+    float yRotation;
+
+    Vector2 lookInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get mouse input
+        lookInput = inputActions.FindAction("Look").ReadValue<Vector2>();
+        float mouseX = lookInput.x * sensX * Time.deltaTime;
+        float mouseY = lookInput.y * sensY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+ 
+    }
+}
---
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/Test_PlayerMovement.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Test_PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] InputActionAsset inputActions;
+
+    CharacterController characterController;
+
+    Vector2 moveInput;
+    Vector3 dirInput;
+
+
+
+    [SerializeField] float walkSpeed = 1f;
+    [SerializeField] float sprintSpeed = 1f;
+
+    bool isRunning;
+    bool isCrouching;
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.FindAction("Run").started += OnStartRun;
+        inputActions.FindAction("Run").canceled += OnStopRun;
+        inputActions.FindAction("Crouch").started += OnStartCrouch;
+        inputActions.FindAction("Crouch").canceled += OnStopCrouch;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+        inputActions.FindAction("Run").started -= OnStartRun;
+        inputActions.FindAction("Run").canceled -= OnStopRun;
+        inputActions.FindAction("Crouch").started -= OnStartCrouch;
+        inputActions.FindAction("Crouch").canceled -= OnStopCrouch;
+    }
+
+    private void OnStartRun(InputAction.CallbackContext ctx)
+    {
+        isRunning = true;
+    }
+    private void OnStopRun(InputAction.CallbackContext ctx)
+    {
+        isRunning = false;
+    }
+
+    private void OnStartCrouch(InputAction.CallbackContext ctx)
+    {
+        isCrouching = true;
+    }
+    private void OnStopCrouch(InputAction.CallbackContext ctx)
+    {
+        isCrouching = false;
+    }
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Crouch();
+    }
+
+    private void Move()
+    {
+        moveInput = inputActions.FindAction("Move").ReadValue<Vector2>();
+        dirInput = new Vector3(moveInput.x, 0, moveInput.y);
+
+        Vector3 move = transform.right * dirInput.x + transform.forward * dirInput.z;
+
+
+        float actualSpeed;
+        if (isRunning) actualSpeed = sprintSpeed;
+        else actualSpeed = walkSpeed;
+
+        characterController.Move(move.normalized * actualSpeed * Time.deltaTime);
+    }
+
+    private void Crouch()
+    {
+        if (isCrouching) characterController.height = 1f;
+        else characterController.height = 2f;
+    }
+}
