f91925d79620e7c52991454413d921cef6c2750c
AuguZer <augumahieu@hotmail.fr>
2023-09-18 14:17:09 +0200

    Fix cc height and center

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerScripts/PlayerCrouch.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerScripts/PlayerCrouch.cs
@@ -9,8 +9,8 @@ public class PlayerCrouch : PlayerBaseState
     {
         playerState.state = PlayerStateManager.PlayerState.Crouch;
         playerState.inputManager.speed = playerState.crouchSpeed;
-        playerState.inputManager.characterController.height = 1;
-        playerState.inputManager.characterController.center = new Vector3(0f, -.5f, 0f);
+        playerState.inputManager.characterController.height = 1.5f;
+        playerState.inputManager.characterController.center = new Vector3(0f, -.25f, 0f);
     }
     public override void OnStateUpdate(PlayerStateManager playerState)
     {
--- INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerScripts/PlayerCrouchIdle.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerScripts/PlayerCrouchIdle.cs
@@ -7,8 +7,8 @@ public class PlayerCrouchIdle : PlayerBaseState
     public override void OnStateEnter(PlayerStateManager playerState)
     {
         playerState.state = PlayerStateManager.PlayerState.CrouchIdle;
-        playerState.inputManager.characterController.height = 1;
-        playerState.inputManager.characterController.center = new Vector3(0f, -.5f, 0f);
+        playerState.inputManager.characterController.height = 1.5f;
+        playerState.inputManager.characterController.center = new Vector3(0f, -.25f, 0f);
     }
     public override void OnStateUpdate(PlayerStateManager playerState)
     {
