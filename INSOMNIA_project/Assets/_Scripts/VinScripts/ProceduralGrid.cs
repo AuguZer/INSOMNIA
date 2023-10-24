6fd0d34406f68f84eeb61054fe9e26cf5de9f172
Rozla <vincent.laroze.vl@gmail.com>
2023-10-24 19:27:40 +0200

    ça continue en procedural grid

--- INSOMNIA_project/Assets/_Scripts/VinScripts/ProceduralGrid.cs
+++ INSOMNIA_project/Assets/_Scripts/VinScripts/ProceduralGrid.cs
@@ -12,6 +12,9 @@ public class ProceduralGrid : MonoBehaviour
     public float cellSize = 1f;
     public Vector3 gridOffset;
     public int gridSize = 4;
+    public int gridSizeX = 4;
+    public int gridSizeY = 4;
+    public int gridSizeZ = 4;
 
     private void Awake()
     {
@@ -21,7 +24,8 @@ public class ProceduralGrid : MonoBehaviour
     // Start is called before the first frame update
     private void Update()
     {
-        MakeDiscreteProceduralGrid();
+        //MakeDiscreteProceduralGrid();
+        MakeDiscreteProcedural3DGrid();
         UpdateMesh();
     }
 
@@ -70,6 +74,53 @@ public class ProceduralGrid : MonoBehaviour
 
             }
 
+        }
+    }
+
+    void MakeDiscreteProcedural3DGrid()
+    {
+        //set array sizes
+        vertices = new Vector3[gridSizeX * gridSizeY * gridSizeZ * 8];
+        triangles = new int[gridSizeX * gridSizeY * gridSizeZ * 36];
+
+        //set tracker integers
+        int v = 0;
+        int t = 0;
+
+        //set vertex offset
+        float vertexOffset = cellSize * .5f;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                for(int z = 0; z < gridSizeZ; z++)
+                {
+                    Vector3 cellOffset = new Vector3(x * cellSize * 1.5f, y * cellSize * 1.5f, z * cellSize * 1.5f);
+
+                    //populate the vertices and triangle arrays
+                    vertices[v] = new Vector3(-vertexOffset, -vertexOffset, -vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 1] = new Vector3(-vertexOffset, -vertexOffset, vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 2] = new Vector3(vertexOffset, -vertexOffset, -vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 3] = new Vector3(vertexOffset, -vertexOffset, vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 4] = new Vector3(-vertexOffset, vertexOffset, -vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 5] = new Vector3(-vertexOffset, vertexOffset, vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 6] = new Vector3(vertexOffset, vertexOffset, -vertexOffset) + cellOffset + gridOffset;
+                    vertices[v + 7] = new Vector3(vertexOffset, vertexOffset, vertexOffset) + cellOffset + gridOffset;
+
+                    triangles[t] = triangles[t + 6] = triangles[t + 32] = v;
+                    triangles[t + 1] = triangles[t + 4] = triangles[t + 8] = triangles[t + 9] = triangles[t + 18] = v + 2;
+                    triangles[t + 2] = triangles[t + 3] = triangles[t + 26] = triangles[t + 30] = triangles[t + 33] = v + 1;
+                    triangles[t + 5] = triangles[t + 20] = triangles[t + 21] = triangles[t + 29] = triangles[t + 24] = v + 3;
+                    triangles[t + 7] = triangles[t + 10] = triangles[t + 12] = triangles[t + 31] = triangles[t + 35] = v + 4;
+                    triangles[t + 11] = triangles[t + 14] = triangles[t + 15] = triangles[t + 19] = triangles[t + 22] = v + 6;
+                    triangles[t + 13] = triangles[t + 16] = triangles[t + 25] = triangles[t + 28] = triangles[t + 34] = v + 5;
+                    triangles[t + 17] = triangles[t + 23] = triangles[t + 27] = v + 7;
+
+                    v += 8;
+                    t += 36;
+                }
+            }
         }
     }
 }
