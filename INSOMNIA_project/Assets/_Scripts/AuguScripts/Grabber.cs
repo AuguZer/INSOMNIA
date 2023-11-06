cbaa78de1aa7d27469973c8b3917eb8d6b006604
AuguZer <augumahieu@hotmail.fr>
2023-11-06 15:20:19 +0100

    Try PickUp objects

---
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/Grabber.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Grabber : MonoBehaviour
+{
+    [SerializeField] InputActionAsset inputActions;
+
+    [Header("Grab Settings")]
+    [SerializeField] Transform grabArea;
+    GameObject heldObj;
+    Rigidbody heldObjRB;
+
+    [Header("Physics Settings")]
+    [SerializeField] float grabRange = 5f;
+    [SerializeField] float grabForce = 150f;
+
+    [SerializeField] bool leftButtonDown;
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.FindAction("Grab").started += OnStartGrab;
+        inputActions.FindAction("Grab").canceled += OnStopGrab;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+        inputActions.FindAction("Grab").started -= OnStartGrab;
+        inputActions.FindAction("Grab").canceled -= OnStopGrab;
+    }
+
+    private void OnStartGrab(InputAction.CallbackContext ctx)
+    {
+        leftButtonDown = true;
+    }
+    private void OnStopGrab(InputAction.CallbackContext ctx)
+    {
+        leftButtonDown = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (leftButtonDown)
+        {
+            if (heldObj == null)
+            {
+                RaycastHit hit;
+
+                if(Physics.Raycast(transform.position, transform.forward, out hit, grabRange))
+                {
+                    //Grab Object
+                    GrabObject(hit.transform.gameObject);
+                }
+            }
+            else
+            {
+                //Drop Object
+                DropObject();
+            }
+        }
+        if (heldObj != null)
+        {
+            //Move Object
+            MoveObject();
+        }
+     
+    }
+
+    private void GrabObject(GameObject grabObj)
+    {
+        if (grabObj.GetComponent<Rigidbody>())
+        {
+            heldObjRB = grabObj.GetComponent<Rigidbody>();
+            heldObjRB.useGravity = false;
+            heldObjRB.drag = 10f;
+            heldObjRB.constraints = RigidbodyConstraints.FreezeRotation;
+
+            heldObjRB.transform.parent = grabArea;
+            heldObj = grabObj;
+        }
+    }
+
+    private void DropObject()
+    {
+            heldObjRB.useGravity = true;
+            heldObjRB.drag = 1f;
+            heldObjRB.constraints = RigidbodyConstraints.None;
+
+            heldObjRB.transform.parent = null;
+            heldObj = null; 
+    }
+
+    private void MoveObject()
+    {
+        if (Vector3.Distance(heldObj.transform.position, grabArea.position)> .1f)
+        {
+            Vector3 moveDirection = (grabArea.position - heldObj.transform.position);
+            heldObjRB.AddForce(moveDirection * grabForce);
+        }
+       
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(grabArea.position, .5f);
+    }
+}
