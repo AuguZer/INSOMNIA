ca85cb5762d4c1b2f296ff8d3c73a14095f5aeb4
AuguZer <augumahieu@hotmail.fr>
2023-11-09 14:24:30 +0100

    Toujours sur les collisions avec la porte

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/Door.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/Door.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] bool open = false;
+    [SerializeField] bool canRotate = true;
+    [SerializeField] bool openRot;
+    [SerializeField] bool closeRot;
     [SerializeField] float openAngle = -90f;
     [SerializeField] float closeAngle = 0f;
     [SerializeField] float smooth = 2f;
+    [SerializeField] float detectionRadius = 1f;
+
+    [SerializeField] LayerMask playerMask;
+
+    [SerializeField] GameObject doorGraphics;
+
+    Quaternion initialRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,15 +31,53 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (open)
+        //CollisionWithPlayer();
+        Debug.Log(CollisionWithPlayer());
+        canRotate = CollisionWithPlayer() ? false : true;
+        canRotate = closeRot ? true: false;
+        canRotate = openRot ? true: false;
+
+        if (open && canRotate)
         {
+            closeRot = false;
             Quaternion targetRotation = Quaternion.Euler(0, openAngle, 0);
             transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
         }
-        else
+        else if (!open && canRotate)
         {
+            openRot = false;
             Quaternion targetRotation1 = Quaternion.Euler(0, closeAngle, 0);
             transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation1, smooth * Time.deltaTime);
         }
+
+        if (transform.localRotation == Quaternion.Euler(0, openAngle, 0))
+        {
+            openRot = true;
+            closeRot = false;
+        }
+        if (transform.localRotation == Quaternion.Euler(0, closeAngle, 0))
+        {
+            openRot = false;
+            closeRot = true;
+        }
+
+    }
+    
+    private bool CollisionWithPlayer()
+    {
+        Collider[] colliders = Physics.OverlapSphere(doorGraphics.transform.position, detectionRadius, playerMask);
+
+        if (colliders.Length > 0) 
+        {
+            initialRotation = transform.localRotation;
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(doorGraphics.transform.position, detectionRadius);
     }
 }
