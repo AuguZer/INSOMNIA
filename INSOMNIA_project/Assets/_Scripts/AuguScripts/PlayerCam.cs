9115ca71b5061252c8203d699ad71911e766f60d
AuguZer <augumahieu@hotmail.fr>
2023-11-28 16:55:02 +0100

    NearClip on Hide

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerCam.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PlayerCam.cs
@@ -36,10 +36,13 @@ public class PlayerCam : MonoBehaviour
     PlayerStateManager playerStateManager;
     PlayerInputManager playerInputManager;
 
+    Camera cam;
+
     private void Awake()
     {
         playerStateManager = GetComponentInParent<PlayerStateManager>();
         playerInputManager = GetComponentInParent<PlayerInputManager>();
+        cam = GetComponent<Camera>();
     }
 
     private void OnEnable()
@@ -238,6 +241,15 @@ public class PlayerCam : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if(playerStateManager.isHiding)
+        {
+            cam.nearClipPlane = 0.01f;
+        }
+        else
+        {
+            cam.nearClipPlane = 0.32f;
+        }
     }
 
     IEnumerator LerpRotationCam(Quaternion startValue, Quaternion endValue, float duration, Vector3 startPos, Vector3 endPos)
