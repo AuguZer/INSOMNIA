f95af6bc00bdf34accb4b150fb128818bea76b9e
AuguZer <augumahieu@hotmail.fr>
2024-02-08 14:46:44 +0100

    save before coroutine

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/EyeBlink.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/EyeBlink.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EyeBlink : MonoBehaviour
 {
+    Animator animator;
+    int blinkNum;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        blinkNum = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if (blinkNum == 0) StartCoroutine(BlinkCoroutine());
+    }
+
+    IEnumerator BlinkCoroutine()
+    {
+        float randomTime = Random.Range(3f, 10f);
+        yield return new WaitForSeconds(randomTime);
+        blinkNum = (int)Random.Range(0f, 2f);
+        animator.SetFloat("BlinkNum", blinkNum);
+        yield return new WaitForSeconds(.1f);
+        blinkNum = 0;
     }
 }
