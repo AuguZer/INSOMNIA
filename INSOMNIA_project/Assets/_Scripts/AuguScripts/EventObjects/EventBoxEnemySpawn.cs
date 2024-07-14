fdff5d9d7251f0e357f97a5891c47c3ed237e3c6
AuguZer <augumahieu@hotmail.fr>
2024-07-14 17:25:34 +0200

    Add more 3D object

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/EventObjects/EventBoxEnemySpawn.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/EventObjects/EventBoxEnemySpawn.cs
@@ -16,6 +16,15 @@ public class EventBoxEnemySpawn : MonoBehaviour
     public Light[] lightsToSwitchOn;
 
     bool hasPlayedmusic;
+
+
+    [Header("LIGHTS FLICKER")]
+    public float minIntensity = 0.2f; 
+    public float maxIntensity = 2f; 
+    public float minFlickerInterval = 0.1f; 
+    public float maxFlickerInterval = 0.5f; 
+    public float steadyLightDuration;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -58,6 +67,7 @@ public class EventBoxEnemySpawn : MonoBehaviour
         foreach (Light light in lightsToSwitchOn)
         {
             light.enabled = true;
+            StartCoroutine(FlickerCycle(light));
         }
     }
 
@@ -66,5 +76,38 @@ public class EventBoxEnemySpawn : MonoBehaviour
         yield return new WaitForSeconds(.6f);
         moodMusicAudioSource.Play();
 
+    }
+
+    IEnumerator FlickerCycle(Light light)
+    {
+        while (true)
+        {
+            // Phase de flicker
+            yield return StartCoroutine(Flicker(light));
+
+            // Phase d'éclairage constant
+            light.intensity = maxIntensity;
+            steadyLightDuration = Random.Range(10f, 20f);
+            yield return new WaitForSeconds(steadyLightDuration);
+        }
+    }
+
+    IEnumerator Flicker(Light light)
+    {
+        float flickerDuration = Random.Range(.5f, 1f); // Durée totale de la phase de flicker
+        float startTime = Time.time;
+
+        while (Time.time - startTime < flickerDuration)
+        {
+            float randomFlickerInterval = Random.Range(minFlickerInterval, maxFlickerInterval);
+
+            // Éteindre la lumière
+            light.intensity = minIntensity;
+            yield return new WaitForSeconds(randomFlickerInterval);
+
+            // Rallumer la lumière
+            light.intensity = maxIntensity;
+            yield return new WaitForSeconds(randomFlickerInterval);
+        }
     }
 }
