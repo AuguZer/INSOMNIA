7b0448303a7bf00c82502b7c717c2837393f0429
AuguZer <augumahieu@hotmail.fr>
2024-07-29 23:25:02 +0200

    chanvge collider add plant on PickUp object

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/PickUpObject.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PickUpObject.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PickUpObject : MonoBehaviour
 {
     [SerializeField] public bool isHeld;
