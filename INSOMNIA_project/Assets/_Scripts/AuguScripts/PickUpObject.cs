5213d5ba8636dbcd6fe6a24ca39ff7c239f735ac
AuguZer <augumahieu@hotmail.fr>
2024-07-31 00:33:36 +0200

    Macth sounds on drop pickUp objects

--- INSOMNIA_project/Assets/_Scripts/AuguScripts/PickUpObject.cs
+++ INSOMNIA_project/Assets/_Scripts/AuguScripts/PickUpObject.cs
@@ -2,14 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
 public class PickUpObject : MonoBehaviour
 {
     [SerializeField] public bool isHeld;
+
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0f;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        rb.angularDrag = 1f;
+
+        audioSource.spatialBlend = 1;
+        audioSource.playOnAwake = false;
     }
 
     // Update is called once per frame
@@ -23,5 +38,11 @@ public class PickUpObject : MonoBehaviour
         {
             isHeld= true;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        audioSource.volume= .5f;
+        audioSource.Play();
     }
 }
