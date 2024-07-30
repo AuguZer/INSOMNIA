182ad0b7f6dac191b4656ef66b59729dc8e5edee
AuguZer <augumahieu@hotmail.fr>
2024-07-31 00:53:01 +0200

    fixing stuff beofr build

--- INSOMNIA_project/Assets/Blinds.cs
+++ INSOMNIA_project/Assets/Blinds.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Blinds : MonoBehaviour
 {
+    [SerializeField] AudioClip openBlind;
+    [SerializeField] AudioClip closeBlind;
 
     public bool isOpen;
 
     Animator animator;
+
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         animator.SetBool("isOpen", isOpen);
+    }
+
+    public void OpenSound()
+    {
+        audioSource.PlayOneShot(openBlind);
+    }
+
+    public void CloseSound()
+    {
+        audioSource.PlayOneShot(closeBlind);
     }
 }
